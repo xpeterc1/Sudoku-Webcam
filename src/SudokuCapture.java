2e587fa8f759873d57f913d9d9ad800b37c23409
xpeterc1 <xpeterc1@users.noreply.github.com>
2015-08-17 14:21:23 -0400

    Create SudokuCapture.java

---
+++ src/SudokuCapture.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+/*Author: Peter Chow
+ * 
+ * 
+ * SudokuCapture will capture images from a webcam, 
+ * using still image capture from the video, another thread will approximate the bounds of the board
+ * and try to scan in the values of the board for the program to use.
+ * After a successful scan, the program will solve the board and display the answer using an overlay over the camera view
+ * 
+ * */
+
+public class SudokuCapture {
+	
+	public static void main (String args[]){
+		init();
+		CaptureWebcam webcam = new CaptureWebcam();
+		webcam.display();
+	}
+	
+	private static void init() {
+		System.out.println("start");
+		String answer = System.getProperty("sun.arch.data.model");
+		if(answer.matches("64")){
+			System.loadLibrary("./build/x64/opencv_java300");
+		}else if(answer.matches("32")){
+			System.loadLibrary("./build/x86/opencv_java300");
+		}else{
+			System.err.println("Your platform is not comptable");
+			System.exit(0);
+		}		
+	}
+
+}
